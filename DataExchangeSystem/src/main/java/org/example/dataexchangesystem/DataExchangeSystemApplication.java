06ea88bd8d6b7c8fee0719e3242d93c01803df01
stejzy <247792@edu.p.lodz.pl>
2024-10-25 13:49:43 +0200

    Basic structure

---
+++ DataExchangeSystem/src/main/java/org/example/dataexchangesystem/DataExchangeSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.dataexchangesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataExchangeSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataExchangeSystemApplication.class, args);
+    }
+
+}
---
+++ DataExchangeSystem/src/test/java/org/example/dataexchangesystem/DataExchangeSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dataexchangesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataExchangeSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
