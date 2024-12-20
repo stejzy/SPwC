81bcf31d0705bd51c16b82334b0ac8d23d7ded3f
Ceendi <247637@edu.p.lodz.pl>
2024-12-20 01:47:37 +0100

    naprawienie corsa

--- DataExchangeSystem/src/main/java/org/example/dataexchangesystem/config/WebConfig.java
+++ DataExchangeSystem/src/main/java/org/example/dataexchangesystem/config/WebConfig.java
@@ -11,7 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
+                .allowedHeaders("Authorization", "Content-Type")
+                .exposedHeaders("Authorization")
                 .allowCredentials(true);
     }
 }
