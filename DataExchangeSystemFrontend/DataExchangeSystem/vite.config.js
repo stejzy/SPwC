73bb23330d08faa29b6dd5888c9635f8a78ce478
Ceendi <247637@edu.p.lodz.pl>
2025-01-22 22:57:43 +0100

    update zeby po usunieciu bootstrap-vue-next dzialalo

--- DataExchangeSystemFrontend/DataExchangeSystem/vite.config.js
+++ DataExchangeSystemFrontend/DataExchangeSystem/vite.config.js
@@ -3,18 +3,13 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import Components from 'unplugin-vue-components/vite'
-import {BootstrapVueNextResolver} from 'bootstrap-vue-next'
 
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
-    Components({
-      resolvers: [BootstrapVueNextResolver()],
-    }),
+    vueDevTools()
   ],
   server: {
     proxy: {
